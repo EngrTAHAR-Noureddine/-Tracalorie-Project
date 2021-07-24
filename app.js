fb9fa33f465a79cc92c6a6434f0f17c801a40372
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-07-24 20:02:03 +0100

    Get  Populate Items

--- app.js
+++ app.js
@@ -31,11 +31,25 @@ const ItemCtrl = (function(){
 
 //UI Controller 
 const UICtrl = (function(){
-    
+    const UISelectors = {
+        itemList : '#item-list'
+    };
     //Public methods
     return {
-
-    }
+        populateItemList : function(items){
+        let html = ``;
+        items.forEach(function(item){
+            html += ` <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+          </li>`; 
+        }
+        );
+        document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
+    } 
 
 })();
 // App Controller 
